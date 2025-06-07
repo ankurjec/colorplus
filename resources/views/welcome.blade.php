06ff1053193eb8e43aaaa9dc00b465d73002bd94
ankurjec <ankurgogoi917@gmail.com>
2025-06-07 15:03:42 +0530

    Folder named changed in PC

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,6 @@
 
     <link href="img/favicon.ico" rel="icon">
 
-    <!-- Google Web Fonts -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
 
